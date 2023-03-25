1ab5fdd4378ca180793d204c5a40d2fcfa6075b0
JoseOrjuela78 <alfredo.orjuela@gmail.com>
2023-03-25 11:00:41 -0500

    login service

--- api/quotes/quotesController.js
+++ api/quotes/quotesController.js
@@ -1,13 +1,11 @@
 const mssql = require('mssql');
 
 module.exports.createQuote = (request, response) => {
-    // const user = request.usuario;
+    const user = request.usuario;
     const req = new mssql.Request();
     const bd = request.body;
     bd.cliente = String(bd.cliente).toLocaleUpperCase();
-    // const seller = user.nombre + " " + user.apellido;
-    const seller = 'Alfredo Orjuela'
-    const user = { "id_usuario": 1 };
+    const seller = user.nombre + " " + user.apellido;
 
     const sql = `EXEC PR_CREATE_COTIZACION '${bd.cliente}','${seller}',${user.id_usuario},'@id_cotizacion OUTPUT','@code OUTPUT', '@message OUTPUT';`;
 
@@ -37,11 +35,9 @@ module.exports.createQuote = (request, response) => {
 };
 
 module.exports.createQuoteDetail = (request, response) => {
-    //const user = request.usuario;
+    const user = request.usuario;
     const req = new mssql.Request();
     const bd = request.body;
-    console.log(bd);
-    const user = { "id_usuario": 1 };
     const sql = `EXEC PR_CREATE_COTIZACION_DET ${bd.id_cotizacion},${bd.id_detalle},${bd.cantidad},${bd.largoCM},${bd.anchoCM},${bd.altoCM},${bd.peso_kg},${user.id_usuario},'@id_cotdetalle OUTPUT','@code OUTPUT', '@message OUTPUT';`;
     req.query(sql, (err, result) => {
 
@@ -69,6 +65,69 @@ module.exports.createQuoteDetail = (request, response) => {
 
 }
 
+module.exports.updateQuoteDetail = (request, response) => {
+
+    const req = new mssql.Request();
+    const bd = request.body;
+    const sql = `EXEC PR_UPDATE_DETQUOTE ${bd.id_cotdetalle}, ${bd.cantidad},${bd.largoCM},${bd.anchoCM},${bd.altoCM},${bd.peso_kg},'@code OUTPUT', '@message OUTPUT';`;
+    req.query(sql, (err, result) => {
+
+        if (err) {
+            return response.status(400).json({
+                ok: false,
+                err: err.originalError.info.message
+            });
+        };
+
+
+
+        const code = parseInt(result.recordsets[0][0].COD);
+        const message = result.recordsets[1][0].MSG;
+        const data = JSON.stringify(result.recordsets[2][0]);
+
+        response.status(code).json({
+            message,
+            data
+        });
+
+
+
+    });
+
+
+}
+
+
+module.exports.deleteQuoteDetail = (request, response) => {
+    const req = new mssql.Request();
+    const id = request.params.id;
+
+    const sql = `DELETE FROM Cotizacionesdetalle WHERE id_cotdetalle = ${id};`;
+    req.query(sql, (err, result) => {
+
+        if (err) {
+            return response.status(400).json({
+                ok: false,
+                err: err.originalError.info.message
+            });
+        };
+
+
+        const code = 200;
+        const message = "ID BORRADO";
+
+
+        response.status(code).json({
+            message
+        });
+
+
+
+    });
+
+
+}
+
 module.exports.generateQuote = (request, response) => {
 
     const req = new mssql.Request();
@@ -83,8 +142,6 @@ module.exports.generateQuote = (request, response) => {
                 err: err.originalError.info.message
             });
         };
-
-
 
         const code = parseInt(result.recordsets[0][0].COD);
         const message = result.recordsets[1][0].MSG;
@@ -105,7 +162,7 @@ module.exports.getQuotes = (request, response) => {
     const req = new mssql.Request();
     const idquote = parseInt(request.params.idquote);
 
-    const sql = `SELECT * FROM Cotizacionesdetalle WHERE id_cotizacion = ${idquote};`;
+    const sql = `EXEC PR_DETQUOTE ${idquote} ,'@code OUTPUT', '@message OUTPUT';`;
 
     req.query(sql, (err, result) => {
 
@@ -119,37 +176,10 @@ module.exports.getQuotes = (request, response) => {
 
         const message = 'GET LISTA COTIZACIONES';
 
-
         response.status(200).json({
             message,
-            list: result.recordsets[0]
-        });
-
-    });
-
-};
-
-module.exports.getBrands = (request, response) => {
-    const req = new mssql.Request();
-
-    const sql = `SELECT * FROM Proveedores WHERE estado = 1;`;
-
-    req.query(sql, (err, result) => {
-
-        if (err) {
-            return response.status(400).json({
-                ok: false,
-                err: err.originalError.info.message
-            });
-        };
-
-
-        const message = 'GET LISTA MARCAS';
-
-
-        response.status(200).json({
-            message,
-            list: result.recordsets[0]
+            list: result.recordsets[0],
+            total: result.recordsets[1][0].TOTAL
         });
 
     });
@@ -158,15 +188,22 @@ module.exports.getBrands = (request, response) => {
 
 module.exports.getRef = (request, response) => {
     const req = new mssql.Request();
-    const brand = request.params.brand;
+    const key = request.params.key;
 
     const sql = `
-    SELECT ld.id_detalle, ld.part_number, h.descripcion  FROM Zona_Proveedor zp
-    INNER JOIN Listadetalle ld
-    ON zp.id_zm = ld.id_zp
+    SELECT
+    ld.id_detalle, 
+    ld.part_number,
+    h.descripcion,
+    p.proveedor
+    FROM Listadetalle ld
     INNER JOIN Herramientas h
-    ON H.id_herramienta = ld.id_herramienta
-    WHERE zp.id_proveedor = '${brand}';
+    ON ld.id_herramienta = h.id_herramienta
+    INNER JOIN Zona_Proveedor zp
+    ON ld.id_zp = zp.id_zp
+    INNER JOIN Proveedores p
+    ON zp.id_proveedor = p.id_proveedor
+    WHERE part_number LIKE '${key}%';
     `;
 
     req.query(sql, (err, result) => {
@@ -190,3 +227,98 @@ module.exports.getRef = (request, response) => {
     });
 
 };
+
+module.exports.closerQuote = (request, response) => {
+
+    const req = new mssql.Request();
+    const bd = request.body;
+
+    const sql = `EXEC PR_INSERT_TOTALES ${bd.id_cotizacion}, '@code OUTPUT', '@message OUTPUT';`;
+    req.query(sql, (err, result) => {
+
+        if (err) {
+            return response.status(400).json({
+                ok: false,
+                err: err.originalError.info.message
+            });
+        };
+
+        const code = parseInt(result.recordsets[0][0].COD);
+        const message = result.recordsets[1][0].MSG;
+        const rows = JSON.stringify(result.recordsets[2]);
+        const total = result.recordsets[3][0].TOTAL;
+
+        response.status(code).json({
+            message,
+            rows,
+            total
+        });
+
+
+
+    });
+
+
+}
+
+module.exports.closeQuoteRows = (request, response) => {
+
+    const req = new mssql.Request();
+    const bd = request.body;
+    const sql = `EXEC PR_UPDATE_TOTALES ${bd.id_cotTotales},${bd.id_cotizacion},'@code OUTPUT', '@message OUTPUT'`;
+
+    req.query(sql, (err, result) => {
+
+        if (err) {
+            return response.status(400).json({
+                ok: false,
+                err: err.originalError.info.message
+            });
+        };
+
+        const code = parseInt(result.recordsets[0][0].COD);
+        const message = result.recordsets[1][0].MSG;
+        const rows = JSON.stringify(result.recordsets[2]);
+
+        response.status(code).json({
+            message,
+            rows
+        });
+
+
+
+    });
+
+}
+
+module.exports.Cpeso = (request, response) => {
+
+    const req = new mssql.Request();
+    const bd = request.body;
+
+    const sql = `EXEC PR_CPESO ${bd.cantidad}, ${bd.peso_kg},${bd.largoCM},${bd.anchoCM},${bd.altoCM},'@code OUTPUT', '@message OUTPUT'`;
+
+    req.query(sql, (err, result) => {
+
+        if (err) {
+            return response.status(400).json({
+                ok: false,
+                err: err.originalError.info.message
+            });
+        };
+
+
+
+        const code = parseInt(result.recordsets[0][0].COD);
+        const message = result.recordsets[1][0].MSG;
+
+        response.status(code).json({
+            code,
+            message
+        });
+
+
+
+    });
+
+}
--- api/quotes/quotesRouters.js
+++ api/quotes/quotesRouters.js
@@ -4,13 +4,16 @@ const quotesController = require('./quotesController');
 const { verificaToken } = require('../common/authorization');
 
 
-router.post('/api/quote', /*[verificaToken],*/ quotesController.createQuote);
-router.post('/api/quote-detail', /*[verificaToken],*/ quotesController.createQuoteDetail);
-router.put('/api/quote-generate' /*, [verificaToken]*/ , quotesController.generateQuote);
-router.get('/api/quote-get/:idquote', /*[verificaToken],*/ quotesController.getQuotes);
-
-router.get('/api/brands-get', /*[verificaToken],*/ quotesController.getBrands);
-router.get('/api/refs-get/:brand', /*[verificaToken],*/ quotesController.getRef);
+router.post('/api/quote', [verificaToken], quotesController.createQuote);
+router.post('/api/quote-detail', [verificaToken], quotesController.createQuoteDetail);
+router.put('/api/quote-detail', [verificaToken], quotesController.updateQuoteDetail);
+router.delete('/api/quote-delete/:id', [verificaToken], quotesController.deleteQuoteDetail);
+router.put('/api/quote-generate', [verificaToken], quotesController.generateQuote);
+router.get('/api/quote-get/:idquote', [verificaToken], quotesController.getQuotes);
+router.get('/api/refs-get/:key', [verificaToken], quotesController.getRef);
+router.post('/api/quote-close', [verificaToken], quotesController.closerQuote);
+router.put('/api/quote-close-row', [verificaToken], quotesController.closeQuoteRows);
+router.put('/api/quote-cpeso', [verificaToken], quotesController.Cpeso);
 
 
 module.exports = router;
